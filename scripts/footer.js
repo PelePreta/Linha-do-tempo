0fe6d2cce6a8a69617fbfb320c5dbfe7b979100f
Stéfany de Oliveira Teixeira <stefanyoliveira.teixeira@estudante.ufjf.br>
2025-12-13 18:15:23 -0300

    versão final do trabalho

---
+++ scripts/footer.js
@@ -0,0 +1,7 @@
+function esconderFooter() {
+   document.getElementById("site-footer").style.display = 'none'
+}
+
+function mostrarFooter() {
+    document.getElementById("site-footer").style.display = 'block'
+}
